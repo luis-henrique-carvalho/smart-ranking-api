49448057b1e766a7822c994279d2f982e7545570
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-01-06 18:56:04 -0300

    Add initial Prisma migration for Player model with unique constraints

---
+++ prisma/migrations/20250106215225_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_email_key" ON "Player"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_phoneNumber_key" ON "Player"("phoneNumber");
