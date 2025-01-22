1631a3645e5f4668a7ff08e934caf785a12d580d
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-01-21 22:31:05 -0300

    Add validation to CreateTeamDto for firstName and description fields

--- src/modules/team/dto/create-team.dto.ts
+++ src/modules/team/dto/create-team.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTeamDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTeamDto {
+  /**
+   * @example TeamName
+   */
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  /**
+   * @example This is a team description
+   */
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
