a9e839a84a0b2c10fe4934d54476c7e24e9e07cf
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-01-29 18:21:56 -0300

    Add global HTTP exception filter to AppModule for centralized error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { IsUniqueConstraint } from './utils/validations/isUniqueValidation/isUnique.class';
+import { HttpExceptionFilter } from './utils/filters/http-exception.filter';
 // modules
 import { UserModule } from './modules/user/user.module';
 import { TeamModule } from './modules/team/team.module';
@@ -10,6 +11,10 @@ import { TeamModule } from './modules/team/team.module';
 @Module({
   imports: [PrismaModule, UserModule, TeamModule],
   controllers: [AppController],
-  providers: [AppService, IsUniqueConstraint],
+  providers: [
+    AppService,
+    IsUniqueConstraint,
+    { provide: 'APP_FILTER', useClass: HttpExceptionFilter },
+  ],
 })
 export class AppModule {}
